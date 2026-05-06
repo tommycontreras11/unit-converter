ad06e932f1229e44f3e05323340230169849a82c
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2026-05-06 16:18:04 -0400

    Refactor unit conversion logic and update HTML structure for dynamic unit selection

--- script.js
+++ script.js
@@ -1,30 +1,183 @@
-const form = document.getElementById("unit-converter-form")
-let convertResult = document.getElementById("convert-result")
+const units = {
+  length: [
+    {
+      name: "millimeter",
+      value: 0.001,
+    },
+    {
+      name: "centimeter",
+      value: 0.01,
+    },
+    {
+      name: "meter",
+      value: 1,
+    },
+    {
+      name: "kilometer",
+      value: 1000,
+    },
+    {
+      name: "inch",
+      value: 0.0254,
+    },
+    {
+      name: "foot",
+      value: 0.3048,
+    },
+    {
+      name: "yard",
+      value: 0.9144,
+    },
+    {
+      name: "mile",
+      value: 1609.344,
+    },
+  ],
+  weight: [
+    {
+      name: "milligram",
+      value: 0.001,
+    },
+    {
+      name: "gram",
+      value: 1,
+    },
+    {
+      name: "kilogram",
+      value: 1000,
+    },
+    {
+      name: "ounce",
+      value: 28.3495,
+    },
+    {
+      name: "pound",
+      value: 453.592,
+    },
+  ],
+  temperature: [
+    {
+      name: "celsius",
+      value: 1,
+    },
+    {
+      name: "fahrenheit",
+      value: 1,
+    },
+    {
+      name: "kelvin",
+      value: 1,
+    },
+  ],
+};
 
-const units = new Map()
-units.set("millimeter", 0.001)
-units.set("centimeter", 0.01)
-units.set("meter", 1)
-units.set("kilometer", 1000)
-units.set("inch", 0.0254)
-units.set("foot", 0.3048)
-units.set("yard", 0.9144)
-units.set("mile", 1609.344)
+let unitToConvertFrom = document.getElementById("unit-to-convert-from");
+let unitToConvertTo = document.getElementById("unit-to-convert-to");
+
+const unit = document.getElementById("unit");
+let unitValue = unit.value;
+
+let unitsSelected = units[unitValue];
+
+unit.addEventListener("change", (event) => {
+  unitValue = event.target.value;
+
+  unitsSelected = units[unitValue];
+  removeChildOptionsFromSelect(
+    unitToConvertFrom.children.length,
+    unitToConvertFrom,
+    unitToConvertTo,
+  );
+  addOptionsToSelects(unitsSelected, name, unitToConvertFrom, unitToConvertTo);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  unitsSelected = units[unitValue];
+  addOptionsToSelects(unitsSelected, name, unitToConvertFrom, unitToConvertTo);
+});
+
+addOptionsToSelects = (
+  unitsSelected,
+  name,
+  unitToConvertFrom,
+  unitToConvertTo,
+) => {
+  for (let i = 0; i < unitsSelected.length; i++) {
+    let name = unitsSelected[i].name;
+
+    unitToConvertFrom.innerHTML +=
+      "<option value=" + name + ">" + name + "</option>";
+    unitToConvertTo.innerHTML +=
+      "<option value=" + name + ">" + name + "</option>";
+  }
+};
+
+removeChildOptionsFromSelect = (
+  unitLength,
+  unitToConvertFrom,
+  unitToConvertTo,
+) => {
+  if (unitLength > 1) {
+    for (let i = unitLength - 1; i > 0; i--) {
+      unitToConvertFrom.removeChild(unitToConvertFrom.children[i]);
+      unitToConvertTo.removeChild(unitToConvertTo.children[i]);
+    }
+  }
+};
+
+const form = document.getElementById("unit-converter-form");
+let convertResult = document.getElementById("convert-result");
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault()
+  event.preventDefault();
 
-    const formData = new FormData(form)
+  const formData = new FormData(form);
 
-    const lengthToConvert = formData.get("length-to-convert")
-    const unitToConvertFrom = units.get(formData.get("unit-to-convert-from"))
-    const unitToConvertTo = units.get(formData.get("unit-to-convert-to"))
+  const lengthToConvert = formData.get("length-to-convert");
+  const unitToConvertFrom = unitsSelected.find(
+    (u) => u.name === formData.get("unit-to-convert-from"),
+  ).value;
+  const unitToConvertTo = unitsSelected.find(
+    (u) => u.name === formData.get("unit-to-convert-to"),
+  ).value;
 
-    let result = (lengthToConvert * unitToConvertFrom) / unitToConvertTo
+  let result = 0;
 
-    if(result >= 1000) result = result.toFixed(2)
-    else if(result >= 1) result = result.toFixed(4)
-    else result = result.toFixed(6)
+  if (unit === "temperature") {
+    if (unitToConvertFrom === "celsius" && unitToConvertTo === "fahrenheit") {
+      result = (lengthToConvert * 9) / 5 + 32;
+    } else if (
+      unitToConvertFrom === "fahrenheit" &&
+      unitToConvertTo === "celsius"
+    ) {
+      result = ((lengthToConvert - 32) * 5) / 9;
+    }
 
-    convertResult.innerText = Number(result).toLocaleString()
-})
+    if (unitToConvertFrom === "celsius" && unitToConvertTo === "kelvin") {
+      result = lengthToConvert + 273.15;
+    } else if (
+      unitToConvertFrom === "kelvin" &&
+      unitToConvertTo === "celsius"
+    ) {
+      result = lengthToConvert - 273.15;
+    }
+
+    if (unitToConvertFrom === "fahrenheit" && unitToConvertTo === "kelvin") {
+      result = ((lengthToConvert - 32) * 5) / 9 + 273.15;
+    } else if (
+      unitToConvertFrom === "kelvin" &&
+      unitToConvertTo === "fahrenheit"
+    ) {
+      result = ((lengthToConvert - 273.15) * 9) / 5 + 32;
+    }
+  } else {
+    result = (lengthToConvert * unitToConvertFrom) / unitToConvertTo;
+  }
+
+  if (result >= 1000) result = result.toFixed(2);
+  else if (result >= 1) result = result.toFixed(4);
+  else result = result.toFixed(6);
+
+  convertResult.innerText = Number(result).toLocaleString();
+  console.log(unit);
+});
